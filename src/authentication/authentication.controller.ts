75847f2d397a1170edbdb4cc644c65e7713fb0cc
PhongNDH <phongndhdev@gmail.com>
2024-08-06 09:53:38 +0700

    change approach to login by google

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,15 +1,24 @@
-import { Controller, Get, HttpCode, HttpStatus, Req, Res, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, HttpStatus, Post, Req, Res, UseGuards } from "@nestjs/common";
 import { AuthenticationService } from "./authentication.service";
+import { OAuth2Client } from "google-auth-library";
 
 import { ApiTags } from "@nestjs/swagger";
 import { GoogleAuthGuard } from "@guard/google-auth.guard";
 import { Request, Response } from "express";
 import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import { LoginDto } from "@authentication/dto/login.dto";
+import { UserEntity } from "@model/user/entities/user.entity";
+
+const client = new OAuth2Client(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET
+);
 
 @ApiTags("Authentication")
 @Controller("auth")
 export class AuthenticationController {
-  constructor(private readonly authenticationService: AuthenticationService, private readonly jwtService: JwtService) {
+  constructor(private readonly authenticationService: AuthenticationService, private readonly jwtService: JwtService, private readonly configService: ConfigService) {
   }
 
   @Get("/google/login")
@@ -24,6 +33,23 @@ export class AuthenticationController {
     const user = req.user;
     console.log(user);
     const token = await this.authenticationService.generateJwt({ ...user });
+    // let frontendUrl = this.configService.getOrThrow("FRONTEND_URL");
+    // frontendUrl += `?token=${encodeURIComponent(token)}&user=${encodeURIComponent(JSON.stringify(user))}`;
+    // res.redirect(frontendUrl);
     res.status(HttpStatus.OK).json({ token: token, user });
+  }
+
+  @Post("/login")
+  async login(@Body() { credential }: { credential: string }): Promise<any> {
+    const ticket = await client.verifyIdToken({
+      idToken: credential,
+      audience: this.configService.getOrThrow("GOOGLE_CLIENT_ID")
+    });
+    const profile = ticket.getPayload();
+    const newLoginDto = new LoginDto(profile.email, profile.sub, profile.picture, profile.name);
+    const user: UserEntity = await this.authenticationService.signIn(newLoginDto);
+    const token = await this.authenticationService.generateJwt({ ...user });
+    return { token };
+
   }
 }
